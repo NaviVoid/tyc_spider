6eaccb5147ffd69817fae9b651fae4408079db8c
Navi <549425867@qq.com>
2023-06-20 15:14:55 +0800

    recursive get_percent

--- src/inv_csv.ts
+++ src/inv_csv.ts
@@ -1,13 +1,18 @@
 import { Inv, Company } from "./db";
 import log from "./log";
 
-const INV_RECORDS: string[] = [];
+// 记录投资的 _id 防止环
+let INV_RECORDS: string[] = [];
 
 const get_percent = async (cid: number, fund_cids: number[]) => {
-  const lv1_funds = await Inv.find({ cid });
+  const lv1_funds = (await Inv.find({ cid })).filter(
+    (inv) => INV_RECORDS.indexOf(inv._id.toString()) === -1
+  );
   if (lv1_funds.length === 0) {
     return [];
   }
+
+  INV_RECORDS = INV_RECORDS.concat(lv1_funds.map((inv) => inv._id.toString()));
 
   const funds = lv1_funds.filter((fund) => fund_cids.indexOf(fund.cid) !== -1);
   const infos = funds.map((fund) => {
@@ -19,14 +24,23 @@ const get_percent = async (cid: number, fund_cids: number[]) => {
     (fund) => fund_cids.indexOf(fund.cid) === -1
   );
 
+  log.info(
+    `[inv_csv] ${cid} has ${funds.length} funds, ${next_funds.length} next funds`
+  );
+
   for (const fund of next_funds) {
-    const next_infos = await get_percent(fund.cid, fund_cids);
+    log.info(`[inv_csv] ${cid} look for next ${fund.owner} funds`);
+
+    const next_infos = await get_percent(fund.owner, fund_cids);
     for (const next_info of next_infos) {
       infos.push({
         owner: next_info.owner,
         percent: fund.percent * next_info.percent,
       });
     }
+    log.info(
+      `[inv_csv] ${cid} look for next ${fund.owner} funds has ${next_infos.length} results`
+    );
   }
 
   return infos;
@@ -51,9 +65,11 @@ const run = async (names: string[]) => {
   log.info(`[inv_csv] companies: ${cpy_cids.length}`);
 
   for (const cid of cpy_cids) {
+    INV_RECORDS = [];
+    log.info(`[inv_csv] ${cid} begin`);
     const inv_infos = await get_percent(cid, fund_cids);
-
     log.info(inv_infos);
+    log.info(`[inv_csv] ${cid} end`);
   }
 };
 
